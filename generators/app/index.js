bceec8862d384bba21266371fb60eabf53b62d7e
MarouaniHani <marouanyhany7@gmail.com>
2019-01-08 13:50:21 +0100

    composing with a Generator class

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,9 +3,8 @@ var Generator = require('yeoman-generator');
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
-        this.helperMethod = function () {
-            console.log('won\'t be called automatically');
-        }
+       // this.argument('appname',{ type: String, required: true});
+        //this.log(this.options.appname);
         // custom code
         this.option('babel');
 
@@ -13,9 +12,11 @@ module.exports = class extends Generator {
     /*method1() {
         this.log('method 1 just ran');
     }
+    //private method
     _method2() {
         this.log('method 2 just ran');
     }*/
+    //using priorityName
     /*end(){
         console.log('end');
     }
@@ -31,7 +32,7 @@ module.exports = class extends Generator {
     default(){
         console.log('def');
     }*/
-    async prompting() {
+   /* async prompting() {
         const answers = await this.prompt([{
           type    : 'input',
           name    : 'name',
@@ -50,5 +51,26 @@ module.exports = class extends Generator {
         this.log('app name', answers.name);
         this.log('cool feature', answers.cool);
         this.log('satisfaction',answers.satis);
+        
+      }*/
+      //using answers at a later stage
+      /*async prompting() {
+        this.answers = await this.prompt([{
+          type    : 'confirm',
+          name    : 'cool',
+          message : 'Would you like to enable the Cool feature?',
+          //Providing a default value will prevent 
+          //the user from returning any empty answers
+          store   : true 
+        }]);
+      }
+    
+      writing() {
+        this.log('cool feature', this.answers.cool); // user answer `cool` used
+      }*/
+      //composing with a Generator class
+      initializing() {
+        this.composeWith(require.resolve('../turbo'));
+        this.composeWith(require.resolve('../electric'));
       }
 };
